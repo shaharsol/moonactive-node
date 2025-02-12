9fef316719bd8d71d7b4a87a050494f01ef175af
shaharsol <shahar@gnift.com>
2025-02-12 15:24:51 +0200

    thenification

--- 06-file-system/app.js
+++ 06-file-system/app.js
@@ -31,16 +31,29 @@ const promisify = (func) => {
 const readFilePromise = promisify(fileSystem.readFile);
 
 // IIFE - imediately invoked function execution
-(async () => {
-    const indexData = await readFilePromise('./index.txt', 'utf-8')
-    const index2Data = await readFilePromise(indexData, 'utf-8')
-    const index3Data = await readFilePromise(index2Data, 'utf-8')
-    console.log(index3Data)
-})()
+// (async () => {
+//     const indexData = await readFilePromise('./index.txt', 'utf-8')
+//     const index2Data = await readFilePromise(indexData, 'utf-8')
+//     const index3Data = await readFilePromise(index2Data, 'utf-8')
+//     console.log(index3Data)
+// })()
+
+// this is called thenification, it was widely used
+// between 2015-2017 until async await arrived
+readFilePromise('./index.txt', 'utf-8').then((indexData) => {
+    return readFilePromise(indexData, 'utf-8')
+}).then((index2Data) => {
+    return readFilePromise(index2Data, 'utf-8')
+}).then((indexData3) => {
+    console.log(indexData3)
+})
 
 // main()
 console.log('started...')
 
+// callback hell example: 
+// this is the only reason humanity invented promises
+//
 // fileSystem.readFile('./index.txt', 'utf-8', (err, data) => {
 //     fileSystem.readFile(data, 'utf-8', (err, data) => {
 //         fileSystem.readFile(data, 'utf-8', (err, data) => {
