57edd3817312b3e31bfdc021fea06265ecb4128e
shaharsol <shahar@gnift.com>
2025-02-16 16:15:32 +0200

    worker aledgedly done

--- 14-crypto-app/src/worker.ts
+++ 14-crypto-app/src/worker.ts
@@ -1,5 +1,9 @@
 import config from 'config'
 import getModel from './models/user-symbols/factory'
+import axios from 'axios'
+import * as cheerio from 'cheerio';
+import redis from './db/redis';
+import { EmitHint } from 'typescript';
 
 function sleep(timeout: number) {
     return new Promise((resolve) => {
@@ -8,13 +12,19 @@ function sleep(timeout: number) {
 }
 
 async function scrapeSymbol(symbol: string) {
-
+    const response = await axios.get<string>(`https://www.google.com/finance/quote/${symbol}-USD`)
+    const html = response.data
+    const $ = cheerio.load(html)
+    const value = $('.YMlKec.fxKbKc').text()
+    
+    await redis.lpush(`company:cryptoapp:symbols:${symbol}`, `${value}::${(new Date()).toISOString()}`)
+    console.log(`pushed to redis the value ${value} for ${symbol}`)
 }
 
 async function work() {
     try {
         const symbolsToScrape = await getModel().getDistinctSymbols()
-        const result = await Promise.allSettled(symbolsToScrape.map(symbolToScrape => scrapeSymbol(symbolToScrape.symbol)))
+        await Promise.allSettled(symbolsToScrape.map(symbolToScrape => scrapeSymbol(symbolToScrape.symbol)))
         await sleep(config.get<number>('worker.interval'))
     } catch (e) {
         console.error(`error in worker`, e)
