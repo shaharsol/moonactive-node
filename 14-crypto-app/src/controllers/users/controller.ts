89738bbf3c82582d23a7c2f9d58219fa87faa3c8
shaharsol <shahar@gnift.com>
2025-02-16 16:31:18 +0200

    embedding symbolValues to the ejs

--- 14-crypto-app/src/controllers/users/controller.ts
+++ 14-crypto-app/src/controllers/users/controller.ts
@@ -1,11 +1,22 @@
 import { NextFunction, Request, Response } from "express";
 import getModel from "../../models/user-symbols/factory";
+import redis from "../../db/redis";
+import config from 'config'
 
 export async function dashboard(req: Request, res: Response, next: NextFunction) {
     try {
         const userId = '09ac3c91-eeab-4388-b8e9-ecbaf3d96466'
         const userSymbols = await getModel().getPerUser(userId)
-        res.render('users/dashboard', { userSymbols })
+
+        const symbolKeyTemplate = config.get<string>('redis.symbolKeyTemplate')
+
+        const result = await Promise.all(userSymbols.map(userSymbol => redis.lrange(`${symbolKeyTemplate}:${userSymbol.symbol}`, 0, 0)))
+        const symbolValues = userSymbols.map((userSymbol, index) => ({
+            symbol: userSymbol.symbol,
+            value: result[index][0].split('::')[0]
+        }))
+
+        res.render('users/dashboard', { userSymbols, symbolValues })
     } catch (e) {
         next(e)
     }
